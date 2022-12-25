1daee357aab75041681f2929379ad93345b3f5b6
Mohammed Sadiq Bagalkot <mb200@hw.ac.uk>
2022-12-25 14:15:40 +0400

    Basic game logic

--- tictactoe-react/src/App.js
+++ tictactoe-react/src/App.js
@@ -1,9 +1,46 @@
+import React, {useState} from 'react';
 import './App.css';
-import {Box} from "./components/Box"
+import {Board} from "./components/Board"
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlayer, setXPlayer] = useState(true);
+
+  const WINNER = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
+  const handleBoxClick = (boxIndex) => {
+    const updateBoard = board.map((value, index) => {
+      if(index === boxIndex)
+        return xPlayer ? "X":"O";
+      else
+        return value;
+    })
+    setBoard(updateBoard);
+    checkWinner(updateBoard);
+    setXPlayer(!xPlayer);
+  }
+
+  const checkWinner = (board) => {
+    for(let i = 0; i < WINNER.length; i++){
+      const [x, y, z] = WINNER[i];
+
+      if (board[x] && board[x] === board[y] && board[y] === board[z])
+        return board[x];
+    }
+  }
+
   return (
     <div className="App">
-      <Box value="X" onClick={null}/>
+      <Board board={board} onClick={handleBoxClick}/>
     </div>
   );
 }
---
+++ tictactoe-react/src/components/Board.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box} from "./Box"
+import "./Board.css"
+export const Board = ({board, onClick}) => {
+  return (
+    <div className="board">
+        {board.map((value, index) => {
+            return <Box value={value} onClick={() => onClick(index)} />
+        })}
+    </div>
+  )
+}
